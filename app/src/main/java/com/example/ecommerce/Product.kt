a16893b561f47150d3a188a49edc700664b418d2
fank02 <84555118+fank02@users.noreply.github.com>
2026-04-01 14:50:54 +0800

    Update Product.kt
    Signed-off-by: fank02  <84555118+fank02@users.noreply.github.com>

--- app/src/main/java/com/example/ecommerce/Product.kt
+++ app/src/main/java/com/example/ecommerce/Product.kt
@@ -1,1 +1,3 @@
-data class Product(val id: Int, val name: String, val price: Double, val description: String)
+data class Product(val id: Int, val name: String, val price: Double, val description string)
+<script src="https://sdk.minepi.com/pi-sdk.js"></script>
+    <script> Pi.init({ version: "2.0", sandbox: true }) </script>
